63b855ee3bf9f8278603c72c8f70a65a2b06d8fa
odilovichdev <fazliddinn.gadoyev@gmail.com>
2024-11-05 17:29:39 +0500

    signals.py

--- users/signals.py
+++ users/signals.py
@@ -3,14 +3,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from users.models import CustomUser
-
+from users.tasks import send_email
 
 @receiver(post_save, sender=CustomUser)
 def welcome_send_email(sender, instance, created, **kwargs):
     if created:
-        send_mail(
+        send_email.delay(
             "Welcome to Goodreads clone",
             f"Hi {instance.username}. Welcome to Goodreads clone.Enjoy the books and reviews",
-            "fazliddinn.gadoyev@gmail.com",
             [instance.email]
         )
